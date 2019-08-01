25ba8f09d3254e30efeef0354889852f4b407ebb
anattoliy90 <anattoliy90@gmail.com>
2019-08-01 10:43:43 +0300

    create simplecomp, change parameters

--- ex2/simplecomp/index.php
+++ ex2/simplecomp/index.php
@@ -1,5 +1,18 @@
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
 
-
+<?$APPLICATION->IncludeComponent(
+	"bitrix:simplecomp.exam",
+	"",
+	Array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"IBLOCK_TYPE" => "products",
+		"IBLOCKS" => array("2"),
+		"DETAIL_URL" => "",
+		"CACHE_TYPE" => "A",
+		"CACHE_TIME" => "180",
+		"CACHE_GROUPS" => "Y",
+		"PARENT_SECTION" => ""
+	)
+);?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ local/components/bitrix/simplecomp.exam/lang/ru/.description.php
@@ -0,0 +1,5 @@
+<?
+$MESS ['NAME'] = "Мой компонент";
+$MESS ['DESCRIPTION'] = "Мой компонент";
+$MESS ['SECTION_NAME'] = "Экзамен №2";
+?>
