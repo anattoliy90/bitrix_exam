c7f6f584e0742a8e5e699dbdf444c4597468d4e3
anattoliy90 <anattoliy90@gmail.com>
2020-01-20 10:31:32 +0300

    ex2-49 done

--- local/components/molotok/simplecomp.exam3/component.php
+++ local/components/molotok/simplecomp.exam3/component.php
@@ -6,7 +6,11 @@ if(!isset($arParams["CACHE_TIME"]))
 $arParams["CATALOG_IBLOCK"] = intval($arParams["CATALOG_IBLOCK"]);
 $arParams["CLASSIFIER_IBLOCK"] = intval($arParams["CLASSIFIER_IBLOCK"]);
 
-if($this->StartResultCache(false, ($USER->GetGroups()))) {	
+if(isset($_REQUEST["F"])) {
+	$this->ClearResultCache($USER->GetGroups());
+}
+
+if($this->StartResultCache(false, ($USER->GetGroups()))) {
 	if(!CModule::IncludeModule("iblock")) {
 		$this->AbortResultCache();
 		ShowError(GetMessage("IBLOCK_MODULE_NOT_INSTALLED"));
@@ -17,8 +21,21 @@ if($this->StartResultCache(false, ($USER->GetGroups()))) {
 	$arr_firm = array();
 	$arr_count = array();
 	$arResult["COUNT"] = 0;
+	$arrFilter = array("IBLOCK_ID" => $arParams["CATALOG_IBLOCK"], "ACTIVE" => "Y", "CHECK_PERMISSIONS" => "Y");
 	
-	$res = CIBlockElement::GetList(array("NAME" => "ASC", "SORT" => "ASC"), array("IBLOCK_ID" => $arParams["CATALOG_IBLOCK"], "ACTIVE" => "Y", "CHECK_PERMISSIONS" => "Y"), false, false, array("ID", "NAME", "IBLOCK_SECTION_ID", "PROPERTY_PRICE", "PROPERTY_MATERIAL", "PROPERTY_ARTNUMBER", "PROPERTY_" . $arParams["PROP_CODE"], "DETAIL_PAGE_URL"));	
+	if(isset($_REQUEST["F"])) {
+		$this->AbortResultCache();
+		
+		$arrFilter_ext = array(array(
+			"LOGIC" => "OR",
+			array("<=PROPERTY_PRICE" => 1700, "PROPERTY_MATERIAL" => "Дерево, ткань"),
+			array("<PROPERTY_PRICE" => 1500, "PROPERTY_MATERIAL" => "Металл, пластик"),
+		));
+		
+		$arrFilter = array_merge($arrFilter, $arrFilter_ext);
+	}
+	
+	$res = CIBlockElement::GetList(array("NAME" => "ASC", "SORT" => "ASC"), $arrFilter, false, false, array("ID", "NAME", "IBLOCK_SECTION_ID", "PROPERTY_PRICE", "PROPERTY_MATERIAL", "PROPERTY_ARTNUMBER", "PROPERTY_" . $arParams["PROP_CODE"], "DETAIL_PAGE_URL"));	
 	$res->SetUrlTemplates($arParams["DETAIL_URL_TEMPLATE"]);
 	while($ob = $res->GetNext())
 	{		
--- local/components/molotok/simplecomp.exam3/templates/.default/template.php
+++ local/components/molotok/simplecomp.exam3/templates/.default/template.php
@@ -1,6 +1,9 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 
 <?if(!empty($arResult["ITEMS"])):?>
+	<div>
+		<?=GetMessage("FILTER");?> <a href="<?=$APPLICATION->GetCurPage(false) . "?F=Y";?>" target="_blank"><?=$APPLICATION->GetCurPage(false) . "?F=Y";?></a>
+	</div>
 	<ul>
 		<?foreach($arResult["ITEMS"] as $firm => $items):?>
 			<li><b><?=$firm;?></b></li>
