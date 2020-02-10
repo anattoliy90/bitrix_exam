09f573d21e0e3e78288fa7ed0783e30195df63f7
anattoliy90 <anattoliy90@gmail.com>
2020-02-10 10:36:48 +0300

    ex2-88 determined the longest page

--- ex2/time_control/index.php
+++ ex2/time_control/index.php
@@ -1,5 +1,6 @@
-<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");?>
 
+Самая ресурсоемкая страница - /products/index.php
+Нагрузка - 13.42%
 
-
-require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
